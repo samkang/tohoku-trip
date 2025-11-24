1159307655ae0da3bda28e1a85d7438af24b1fad
samkang <samkang@infotech-consultant.com>
2025-11-24 20:57:09 +0800

    Refactor: 改用本地 Tailwind CSS 取代 CDN，解決 PWA 離線樣式遺失問題

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ['"Noto Serif TC"', 'serif'],
+        sans: ['"Noto Sans TC"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [
+    require("tailwindcss-animate"),
+  ],
+}
+
